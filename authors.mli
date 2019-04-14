07e1207124bf9ab1816e2686c0261b1fcf5d1def
jasonh90 <jasonh402@gmail.com>
2019-04-13 21:55:26 -0400

    c

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(**
+   Metadata about this submission.
+   @author Jason Huang (jh2424), Yuna Shin (ys457), Olivia Kaiafas (omk8)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
