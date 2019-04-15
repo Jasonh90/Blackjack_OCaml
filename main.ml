464d3242f96cfe8ac14f4987149a12ac3b4d50b6
jasonh90 <jasonh402@gmail.com>
2019-04-15 15:12:37 -0400

    colorful

--- main.ml
+++ main.ml
@@ -1,38 +1,81 @@
-let rank_style = function
-| 
+let suit_style = function
+  | "Spades" -> "\xE2\x99\xA0"
+  | "Clubs" -> "\xE2\x99\xA3"
+  | "Hearts" -> "\xE2\x99\xA5"
+  | "Diamonds" -> "\xE2\x99\xA6"
+  | _ -> failwith "Wrong suit"
 
-let card_style num = 
+let card_style num s = 
+  let suit = suit_style s in 
   let next_line () = ANSITerminal.(print_string [Reset] 
-                                     "\n") in
+                                     "\n") in 
+  let one_suit () = ANSITerminal.(print_string [on_black; Foreground White] 
+                                    ("    " ^ suit ^ "     ")); next_line () in
+  let two_suit () = ANSITerminal.(print_string [on_black; Foreground White] 
+                                    ("   " ^ suit ^ " " ^ suit ^ "    ")); next_line () in
+  let space () = ANSITerminal.(print_string [on_black; Foreground White] 
+                                 "          "); next_line () in 
+  let top_number () = 
+    ANSITerminal.(print_string [on_black; Foreground White] 
+                    (" " ^ string_of_int(num) ^ "       "^(if num < 10 then " " else ""))); next_line () in
+  let top_num n = 
+    ANSITerminal.(print_string [on_black; Foreground White] 
+                    (" " ^ n ^ "        ")); next_line () in
+  let bottom_number () = 
+    ANSITerminal.(print_string [on_black; Foreground White] 
+                    ("       "^(if num < 10 then " " else "") ^ string_of_int(num) ^ " ")); next_line (); next_line () in 
+  let bot_num n = 
+    ANSITerminal.(print_string [on_black; Foreground White] 
+                    ("        " ^ n ^ " ")); next_line (); next_line () in 
   match num with 
-  | 1 -> ANSITerminal.(print_string [on_black; Foreground White] 
-                         (" " ^ string_of_int(num) ^ "      "));
-    next_line ();
-    for i = 0 to 2 do
-      if i = 1 then 
-        ANSITerminal.(print_string [on_black; Foreground White] 
-                        "   \xE2\x99\xA0    ")
-      else
-        ANSITerminal.(print_string [on_black; Foreground White] 
-                        "        "); next_line ()
-    done;
-
-    ANSITerminal.(print_string [on_black; Foreground White] 
-                    ("      " ^ string_of_int(num) ^ " ")); next_line ()
-  | 6 -> ANSITerminal.(print_string [on_black; Foreground White] 
-                         "6       "); next_line ();
-    for i = 0 to 2 do
-      ANSITerminal.(print_string [on_black; Foreground White] 
-                      "  \xE2\x99\xA0 \xE2\x99\xA0   "); next_line ();
-    done;
-    ANSITerminal.(print_string [on_black; Foreground White] 
-                    "      6 "); next_line ();
+  | 1 -> top_number ();
+    for i = 0 to 4 do
+      if i = 2 then one_suit ()
+      else space ()
+    done; bottom_number ()
+  | 2 -> top_number ();
+    for i = 0 to 4 do
+      if i mod 2 = 1 then one_suit () else space () done; 
+    bottom_number ()
+  | 3 -> top_number ();
+    for i = 0 to 4 do 
+      if i mod 2 = 0 then one_suit () else space () done; 
+    bottom_number ()
+  | 4 -> top_number ();
+    for i = 0 to 4 do 
+      if i mod 2 = 1 then two_suit () else space () done; 
+    bottom_number ()
+  | 5 -> top_number (); 
+    two_suit (); space (); one_suit (); space (); two_suit (); 
+    bottom_number ()
+  | 6 -> top_number ();
+    for i = 0 to 4 do
+      if i mod 2 = 0 then two_suit () else space () done;
+    bottom_number ()
+  | 7 -> top_number ();
+    two_suit (); one_suit (); two_suit (); space (); two_suit ();
+    bottom_number ()
+  | 8 -> top_number ();
+    for i = 0 to 4 do
+      if i <> 2 then two_suit () else space () done;
+    bottom_number ()
+  | 9 -> top_number ();
+    for i = 0 to 4 do
+      if i <> 1 then two_suit () else one_suit () done;
+    bottom_number ()
+  | 10 -> top_number ();
+    for i = 0 to 4 do
+      two_suit () done;
+    bottom_number ()
+  | 11 -> top_num "J"; for i = 0 to 4 do if i = 0 || i = 4 then one_suit () else space () done; bot_num "J"
+  | 12 -> top_num "Q"; for i = 0 to 4 do if i = 0 || i = 4 then one_suit () else space () done; bot_num "Q"
+  | 13 -> top_num "K"; for i = 0 to 4 do if i = 0 || i = 4 then one_suit () else space () done; bot_num "K"
   | _ -> failwith "Wrong number"
 
 
 (** [play_game f] . *)
 let play_game num =
-  card_style num
+  for i = 1 to 13 do card_style i "Spades";card_style i "Clubs";card_style i "Diamonds";card_style i "Hearts"done
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
