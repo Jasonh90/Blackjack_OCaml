de006df4c8cb6549baf79135b9abf79c69919487
ys457 <ys457@cornell.edu>
2019-04-18 15:32:20 -0400

    add documentation

--- ai.ml
+++ ai.ml
@@ -1,9 +1,10 @@
 open State
 open Game
 
-(** [dealer state] determines the moves for dealer. When dealer has cards totaling to
-    16 or less, dealer calls hit. If not, dealer calls check *)
+(** [dealer state] determines the moves for dealer. When dealer has cards 
+    totaling to 16 or less, dealer calls hit. If not, dealer calls check. *)
 let dealer state = 
-  let hand = get_hand_of_current state in
+  let current = get_current_player_name state in
+  let hand = get_player_hand state current in
   let score = calculate_score hand in
   if score <= 17 then hit state else check state
